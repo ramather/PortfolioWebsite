e0738035d50783abcdfa301d7d112af7b3832bcf
Ross Mather <ramather96@gmail.com>
2022-04-07 07:42:46 -0400

    created mobile card

--- src/components/works/card mobile/MobileCard.jsx
+++ src/components/works/card mobile/MobileCard.jsx
@@ -6,13 +6,41 @@ import { motion, useMotionValue, useTransform, useAnimation } from "framer-motio
 
 
 const MobileCard = () => {
+    const motionValue = useMotionValue(0);
+    const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
+    const opacityValue = useTransform(
+        motionValue,
+        [-200, -150, 0, 150, 200],
+        [0, 1, 1, 1, 0]
+      );
+      const animControls = useAnimation();
+
   return (
     <div className="c">
       <h3 className="title">Projects</h3>
       
-      <div className="c-card">
+      <motion.div className="c-card"
+       drag="x"
+       x={motionValue}
+       rotate={rotateValue}
+       opacity={opacityValue}
+       dragConstraints={{ left: -1000, right: 1000 }}
+       onDragEnd={(event, info) => {
+         // If the card is dragged only upto 150 on x-axis
+         // bring it back to initial position
+         if (Math.abs(info.point.x) <= 150) {
+           animControls.start({ x: 0 });
+         } else {
+           // If card is dragged beyond 150
+           // make it disappear
+           // making use of ternary operator
+           animControls.start({ x: info.point.x < 0 ? -200 : 200 });
+         }
+       }}
+      
+      >
     
-      </div>
+      </motion.div>
      
     </div>
   );
