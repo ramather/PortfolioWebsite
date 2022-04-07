18c6592e91ad1bb236161337a375429979950de5
Ross Mather <ramather96@gmail.com>
2022-04-07 09:14:19 -0400

    made some cards work good

--- src/components/works/card mobile/MobileCard.jsx
+++ src/components/works/card mobile/MobileCard.jsx
@@ -1,47 +1,50 @@
 import React from "react";
+import move from "lodash-move";
+
 import "./mobileCard.scss";
-import { motion, useMotionValue, useTransform, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
+import { data } from "./data";
 
-// import {  useMotionValue, useTransform, useAnimation } from "framer";
+const CARD_OFFSET = 10;
+const SCALE_FACTOR = 0.06;
 
-
-const MobileCard = () => {
-    const motionValue = useMotionValue(0);
-    const rotateValue = useTransform(motionValue, [-200, 200], [-50, 50]);
-    const opacityValue = useTransform(
-        motionValue,
-        [-200, -150, 0, 150, 200],
-        [0, 1, 1, 1, 0]
-      );
-      const animControls = useAnimation();
+function MobileCard() {
+  const [cards, setCards] = React.useState(data);
+  const moveToEnd = (from) => {
+    setCards(move(cards, from, cards.length - 1));
+  };
 
   return (
     <div className="c">
       <h3 className="title">Projects</h3>
-      
-      <motion.div className="c-card"
-       drag="x"
-       x={motionValue}
-       rotate={rotateValue}
-       opacity={opacityValue}
-       dragConstraints={{ left: -1000, right: 1000 }}
-       onDragEnd={(event, info) => {
-         // If the card is dragged only upto 150 on x-axis
-         // bring it back to initial position
-         if (Math.abs(info.point.x) <= 150) {
-           animControls.start({ x: 0 });
-         } else {
-           // If card is dragged beyond 150
-           // make it disappear
-           // making use of ternary operator
-           animControls.start({ x: info.point.x < 0 ? -200 : 200 });
-         }
-       }}
-      
-      >
-    
-      </motion.div>
-     
+
+
+      <ul className="container">
+        {cards.map((card, index) => {
+          const canDrag = index === 0;
+          return (
+            <motion.li
+              className="c-card"
+              style={{
+                backgroundColor: card.backgroundColor,
+                cursor: canDrag ? "grab" : "auto",
+              }}
+
+              animate={{
+                top: index * -CARD_OFFSET,
+                scale: 1 - index * SCALE_FACTOR,
+                zIndex: cards.length - index,
+              }}
+              drag={canDrag ? "y" : false}
+              dragConstraints={{
+                top: 0,
+                bottom: 0,
+              }}
+              onDragEnd={() => moveToEnd(index)}
+            ></motion.li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
