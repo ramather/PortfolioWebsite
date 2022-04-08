73a72f2fc12c737bfd6b8a37a24c8853dabda86a
Ross Mather <ramather96@gmail.com>
2022-04-08 05:42:47 -0400

    centered finally

--- src/components/works/card mobile/MobileCard.jsx
+++ src/components/works/card mobile/MobileCard.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import move from "lodash-move";
 
 import "./mobileCard.scss";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { data } from "./data";
 
 const CARD_OFFSET = 10;
 const SCALE_FACTOR = 0.06;
 
 function MobileCard() {
-  const [cards, setCards] = React.useState(data);
+  const [cards, setCards] = useState(data);
   const moveToEnd = (from) => {
     setCards(move(cards, from, cards.length - 1));
   };
 
+  const controls = useAnimation();
+
+  useEffect(() => {
+    controls.start((i) => ({
+      opacity: 0,
+      x: 100,
+      transition: { delay: i * 0.3 },
+    }));
+  }, [cards]);
+
   return (
     <div className="c">
       <h3 className="title">Projects</h3>
-
 
       <ul className="container">
         {cards.map((card, index) => {
@@ -27,20 +36,19 @@ function MobileCard() {
               style={{
                 backgroundColor: card.backgroundColor,
               }}
-
               animate={{
                 top: index * -CARD_OFFSET,
                 scale: 1 - index * SCALE_FACTOR,
                 zIndex: cards.length - index,
+                controls
               }}
-              
             ></motion.li>
           );
         })}
       </ul>
-      <button onClick={()=> moveToEnd(0)}>Next</button>
+      <button onClick={() => moveToEnd(0)}>Next</button>
     </div>
   );
-};
+}
 
 export default MobileCard;
