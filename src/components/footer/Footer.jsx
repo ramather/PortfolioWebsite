529646bcd21409863d9a8b37ee3c95b17ddf91c2
Ross Mather <ramather96@gmail.com>
2022-04-08 10:42:47 -0400

    foot fun

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,15 +1,21 @@
-import './footer.scss'
+import "./footer.scss";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <div className='contact'>
-                <h1>CONTACT ME</h1>
-            </div>
-            <div className='links'></div>
-            
+  return (
+    <div className="footer">
+      <div className="contact">
+        <h1>CONTACT ME</h1>
+        <button>Let's talk</button>
+      </div>
+      <div className="links">
+        <div className="technologies">
+          <i id="icon" class="devicon-linkedin-plain"></i>
+          <i id="icon" class="devicon-github-original"></i>
         </div>
-    )
-}
+      </div>
+      <hr></hr>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/works/cards/cards.jsx
+++ src/components/works/cards/cards.jsx
@@ -50,11 +50,10 @@ export default function Cards() {
             </div>
             <div className="right">
               <img src={card.img} />
-              <div className="buttons"> 
-                   <button className="bn5"> Git Repo </button >
-                  <button className="bn5"> Link to Site </button > 
-
-              </div> 
+              <div className="buttons">
+                <button className="bn5"> Git Repo </button>
+                <button className="bn5"> Link to Site </button>
+              </div>
             </div>
           </motion.li>
         );
